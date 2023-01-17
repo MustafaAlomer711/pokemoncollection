3022a845195645b95e08e2bd27cad98a81de8b69
Mustafa Alomer <mustafa_05@msn.com>
2023-01-17 07:56:44 +0000

    got the all relevant cards in the index to show depending on a speccifc set ID

--- app/controllers/pokemon_cards_controller.rb
+++ app/controllers/pokemon_cards_controller.rb
@@ -1,10 +1,24 @@
 class PokemonCardsController < ApplicationController
+  before_action :set_pokemon_card, only: [:show]
 
   def index
     @pokemon_cards = PokemonCard.where(pokemon_set_id: params[:pokemon_set_id])
   end
 
   def show
+  end
+
+  def new
+    @pokemon_card = PokemonCard.new
+  end
+
+  private
+
+  def pokemon_card_params
+    params.require(:pokemon_card).permit(:name, :pokemon_set_id, :artist, :image_url)
+  end
+
+  def set_pokemon_card
     @pokemon_card = PokemonCard.find(params[:id])
   end
 end
